5b722ccc0e0c542944d5cdc6ad5d57f85b664531
Anique Tahir <aniquetahir@gmail.com>
2015-01-23 16:07:31 +0500

    initial commit

---
+++ bulk-redirect.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Name: Bulk Redirect
+ * Description: Allows the addition of redirect urls in bulk
+ * Version: 1.0.0
+ * Author: Anique Rogers Tahir
+ * Author URI: mailto:aniquetahir@gmail.com
+ * Network: true
+ * License: GPL2
+ */
+/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+//Disallow direct access
+defined('ABSPATH') or die('Not Found');
+
+add_action('activate_bulk-redirect/bulk-redirect.php','bulk_redirect_install');
+add_action('wp_footer','log_execution');
+
+function log_execution(){
+	echo 'loggded';
+}
+
+function bulk_redirect_install(){
+	global $wpdb;
+	$table = $wpdb->prefix.'bulk_redirect';
+	$structure = "
+		create table $table (
+			urlfrom varchar(2048),
+			urlto varchar(2048)
+			PRIMARY KEY (urlfrom,urlto) USING BTREE
+		)
+	";
+
+	$wpdb->query($structure);
+}
+
+function add_redirect(){
+	global $wpdb;
+
+}
+
+function remove_redirect(){
+	global $wpdb;
+
+}
+
+function get_redirect(){
+	global $wpdb;
+	$table = $wpdb->prefix.'bulk_redirect';
+	$redirects = $wpdb->get_results("SELECT * FROM $table");
+
+	return $redirects;
+}
+
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Bulk Redirect ===
+Contributors: aniquetahir
+Tags: bulk, redirect, redirection
+Requires at least: 3.1
+Tested up to: 0.0.0
+Stable tag: 1.0.0
+License: GPLv2 or later
+
+Redirects specified urls to other specified urls
+
+== Description ==
+
+Add urls for redirection in Bulk
+
+== Installation ==
+
+Upload and Activate
+
+
+
+== Changelog ==
+
+= 1.0.0 =
+*Release Date - 23rd January, 2015*
+
+* Initial Release
+
+* Fixed a key problem affecting the stats feature in WordPress MU
+* Provide additional blog information in Akismet API calls
